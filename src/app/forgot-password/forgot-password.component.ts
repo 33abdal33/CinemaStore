a60b38e6a64f69069aae604b62ec3574dfddad87
Abdal Lizano <abdallizanomuriel08@gmail.com>
2025-01-29 18:20:15 -0500

    Update Forgot password

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { SimpleHeaderComponent } from "../Shared/components/simple-header/simple-header.component";
+import { FooterComponent } from "../Shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-forgot-password',
   standalone: true,
-  imports: [],
+  imports: [SimpleHeaderComponent, FooterComponent],
   templateUrl: './forgot-password.component.html',
   styleUrl: './forgot-password.component.css'
 })
