396278c0e731e0e5904361910047b95863325074
Abdal Lizano <abdallizanomuriel08@gmail.com>
2025-01-29 18:20:33 -0500

    add login component in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './Home/home.component';
 import { FooterComponent } from './Shared/components/footer/footer.component';
+import { LoginComponent } from "./login/login.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent, FooterComponent],
+  imports: [HomeComponent, FooterComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
