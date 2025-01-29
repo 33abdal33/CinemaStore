0205429afcf2aa3f31e9a0ceb8da80c7d8e293e5
Abdal Lizano <abdallizanomuriel08@gmail.com>
2025-01-29 18:20:51 -0500

    Update login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { SimpleHeaderComponent } from "../Shared/components/simple-header/simple-header.component";
+import { FooterComponent } from "../Shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [SimpleHeaderComponent, FooterComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
