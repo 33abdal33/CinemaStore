7030f920ff0f3d13626b8514a2562db6fd317af0
Abdal Lizano <abdallizanomuriel08@gmail.com>
2025-01-29 06:26:15 -0500

    Update Home Component

--- src/app/Home/home.component.ts
+++ src/app/Home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from "@angular/core";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "../Shared/components/footer/footer.component";
+import { EventCardComponent } from "../Shared/components/event-card/event-card.component";
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [HeaderComponent, FooterComponent],
+    imports: [HeaderComponent, FooterComponent, EventCardComponent],
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
 })
