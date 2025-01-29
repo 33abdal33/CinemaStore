4597b9636f01660bfbe40caaff298aa122d7a45e
Abdal Lizano <abdallizanomuriel08@gmail.com>
2025-01-29 18:19:56 -0500

    Update Event detail

--- src/app/event-detail/event-detail.component.ts
+++ src/app/event-detail/event-detail.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { SimpleHeaderComponent } from "../Shared/components/simple-header/simple-header.component";
+import { FooterComponent } from "../Shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-event-detail',
   standalone: true,
-  imports: [],
+  imports: [SimpleHeaderComponent, FooterComponent],
   templateUrl: './event-detail.component.html',
   styleUrl: './event-detail.component.css'
 })
