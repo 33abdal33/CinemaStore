51ee75ad33d27b87bdbe4a0bf2ba6b6f6e2b1a47
Abdal Lizano <abdallizanomuriel08@gmail.com>
2025-01-29 18:21:09 -0500

    Update register

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { SimpleHeaderComponent } from "../Shared/components/simple-header/simple-header.component";
+import { FooterComponent } from "../Shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [SimpleHeaderComponent, FooterComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
