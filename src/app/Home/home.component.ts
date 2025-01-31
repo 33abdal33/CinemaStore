773d764d508bd6f90774f4ef2f3d7b06963ea261
Abdal Lizano <abdallizanomuriel08@gmail.com>
2025-01-31 02:26:46 -0500

    Basic Implementation on home api in home

--- src/app/Home/home.component.ts
+++ src/app/Home/home.component.ts
@@ -3,6 +3,7 @@ import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "../Shared/components/footer/footer.component";
 import { EventCardComponent } from "../Shared/components/event-card/event-card.component";
 import { HomeService } from "./home.service";
+import { emptyMovie, Movie } from "../Shared/models/movie.model";
 
 @Component({
     selector: 'app-home',
@@ -15,9 +16,19 @@ import { HomeService } from "./home.service";
 export class HomeComponent implements OnInit {
     homeService = inject(HomeService);
 
+    movieTest1: Movie = emptyMovie;
+    movieTest2: Movie = emptyMovie;
+    movieTest3: Movie = emptyMovie;
+    movieTest4: Movie = emptyMovie;
+
     ngOnInit() {
         this.homeService.getData().subscribe((response) => {
             console.log(response);
+            this.movieTest1 = response.movies[0];
+            this.movieTest2 = response.movies[1];
+            this.movieTest3 = response.movies[2];
+            this.movieTest4 = response.movies[3];
+
         });
     }
 }
