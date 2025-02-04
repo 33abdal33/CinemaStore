2b07d2a3ac99c6b1b9a58387414df7f3b9f24e7e
Abdal Lizano <abdallizanomuriel08@gmail.com>
2025-02-04 02:37:17 -0500

    Implement Forgot Password

--- src/app/forgot-password/forgot-password.component.ts
+++ src/app/forgot-password/forgot-password.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { SimpleHeaderComponent } from "../Shared/components/simple-header/simple-header.component";
 import { FooterComponent } from "../Shared/components/footer/footer.component";
+import { FormsModule, NgForm } from '@angular/forms';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-forgot-password',
   standalone: true,
-  imports: [SimpleHeaderComponent, FooterComponent],
+  imports: [SimpleHeaderComponent, FooterComponent, FormsModule, RouterLink],
   templateUrl: './forgot-password.component.html',
   styleUrl: './forgot-password.component.css'
 })
 export class ForgotPasswordComponent {
-
+  onSubmit(email: string) { }
 }
