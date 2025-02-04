cd8a4595c1ef5f44cab2cc6b76c9078b9b478eac
Abdal Lizano <abdallizanomuriel08@gmail.com>
2025-02-04 02:36:27 -0500

    Implement Login Form

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { SimpleHeaderComponent } from "../Shared/components/simple-header/simple-header.component";
 import { FooterComponent } from "../Shared/components/footer/footer.component";
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [SimpleHeaderComponent, FooterComponent],
+  imports: [SimpleHeaderComponent, FooterComponent, FormsModule, ReactiveFormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+    ]),
+  });
 
+  router = inject(Router);
+
+  login() {
+    const email = this.loginForm.controls.email.value!;
+    const password = this.loginForm.controls.password.value!;
+  }
 }
