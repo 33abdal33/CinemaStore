b8ec5b14564acc86ebdcfb9c5467cd401fde0a80
Abdal Lizano <abdallizanomuriel08@gmail.com>
2025-02-07 00:05:26 -0500

    Create Highlightable directive

--- src/app/Shared/directives/highlightable.directive.ts
+++ src/app/Shared/directives/highlightable.directive.ts
@@ -1,11 +1,20 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlightable]',
   standalone: true
 })
 export class HighlightableDirective {
+  constructor(public el: ElementRef) { }
 
-  constructor() { }
+  @HostListener('mouseenter') onEnter() {
+    this.el.nativeElement.style.transform = 'scale(1.05)';
+    this.el.nativeElement.style.boxShadow = 'var(--primary-color) 0px 0px 10px';
+    this.el.nativeElement.style.transition = 'all 300ms';
+  }
 
+  @HostListener('mouseleave') onLeave() {
+    this.el.nativeElement.style.transform = 'scale(1)';
+    this.el.nativeElement.style.boxShadow = 'unset';
+  }
 }
