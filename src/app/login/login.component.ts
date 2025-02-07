79dfba614b94d37732b6e65bc4796b8115701050
Abdal Lizano <abdallizanomuriel08@gmail.com>
2025-02-07 00:14:46 -0500

    update Login Component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,15 +3,17 @@ import { SimpleHeaderComponent } from "../Shared/components/simple-header/simple
 import { FooterComponent } from "../Shared/components/footer/footer.component";
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../Shared/Service/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [SimpleHeaderComponent, FooterComponent, FormsModule, ReactiveFormsModule, RouterLink],
+  imports: [SimpleHeaderComponent, FooterComponent, ReactiveFormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [
@@ -20,10 +22,24 @@ export class LoginComponent {
     ]),
   });
 
+  authService = inject(AuthService);
   router = inject(Router);
 
   login() {
     const email = this.loginForm.controls.email.value!;
     const password = this.loginForm.controls.password.value!;
+    this.authService.login(email, password).subscribe((response) => {
+      if (response && response.success) {
+        localStorage.setItem('token', response.data.token);
+        this.authService.loggedIn.set(true);
+        alert('Login Exitoso' + '\n' + 'Bienvenido a CineMax');
+        const isAdministrator = email === 'admin@gmail.com';
+        this.authService.isAdministrator.set(isAdministrator);
+        localStorage.setItem('isAdministrator', isAdministrator.toString());
+        this.router.navigate([isAdministrator ? '/admin' : '/customer']);
+      } else {
+        alert('Login Fallido' + '\n' + 'Revisa tus credenciales');
+      }
+    });
   }
-}
+};
