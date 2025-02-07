6405ddb32087d2a4afa0f2df7276ec2a4ff67cb2
Abdal Lizano <abdallizanomuriel08@gmail.com>
2025-02-07 00:39:43 -0500

    update Header component

--- src/app/Home/header/header.component.ts
+++ src/app/Home/header/header.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@Angular/material/button';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../Shared/Service/auth.service';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { SearchBarService } from '../services/search-bar.service';
+import { debounceTime, map, startWith } from 'rxjs';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatButtonModule, RouterLink],
+  imports: [MatButtonModule, RouterLink, ReactiveFormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  searchBarFormControl = new FormControl('');
+  searchBarService = inject(SearchBarService);
+  authService = inject(AuthService);
 
+  constructor() {
+    this.searchBarService.currentValue$ =
+      this.searchBarFormControl.valueChanges.pipe(
+        map((value) => (value ? value : '')),
+        debounceTime(300),
+        startWith('')
+      );
+  }
 }
