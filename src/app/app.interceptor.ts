790e74df7ed2fe658cad0396efe31455f9fef258
Abdal Lizano <abdallizanomuriel08@gmail.com>
2025-02-07 01:12:12 -0500

    Create Interceptor

---
+++ src/app/app.interceptor.ts
@@ -0,0 +1,33 @@
+import { HttpInterceptorFn } from '@angular/common/http';
+import { inject, isDevMode } from '@angular/core';
+import { tap } from 'rxjs';
+import { AuthService } from './Shared/Service/auth.service';
+
+export const loggerInterceptor: HttpInterceptorFn = (req, next) => {
+  if (isDevMode()) {
+    console.log('Request URL:', req.url);
+    if (req.method === 'POST') {
+      console.log('Request Body:', req.body)
+    }
+  }
+  return isDevMode()
+    ? next(req).pipe(tap((res) => console.log('Response:', res)))
+    : next(req);
+};
+
+export const jwtInterceptor: HttpInterceptorFn = (req, next) => {
+  let clonedRequest = req;
+  const token = localStorage.getItem('token');
+  if (token) {
+    const auth = inject(AuthService);
+    auth.loggedIn.set(true);
+    auth.isAdministrator.set(false);
+    clonedRequest = req.clone(
+      {
+        headers: req.headers.set('Authorization', `Bearer ${token}`),
+      }
+    );
+
+  }
+  return next(clonedRequest);
+}
