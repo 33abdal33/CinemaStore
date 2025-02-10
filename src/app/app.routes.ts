86f39050108f76c9d63520053f48b51c3abdb0b6
Abdal Lizano <abdallizanomuriel08@gmail.com>
2025-02-09 23:09:11 -0500

    Update Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,4 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './Home/home.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
 
 export const routes: Routes = [
     {
@@ -11,24 +8,113 @@ export const routes: Routes = [
     },
     {
         path: 'home',
+        pathMatch: 'full',
         loadComponent: () =>
             import('./Home/home.component').then((m) => m.HomeComponent),
     },
     {
         path: 'login',
+        pathMatch: 'full',
         loadComponent: () =>
             import('./login/login.component').then((m) => m.LoginComponent),
     },
     {
         path: 'Register',
+        pathMatch: 'full',
         loadComponent: () =>
             import('./register/register.component').then((m) => m.RegisterComponent),
     },
     {
         path: 'forgot-password',
+        pathMatch: 'full',
         loadComponent: () =>
             import('./forgot-password/forgot-password.component').then(
                 (m) => m.ForgotPasswordComponent
             ),
+    },
+    {
+        path: 'event-detail/:id',
+        pathMatch: 'full',
+        loadComponent: () =>
+            import('./event-detail/event-detail.component').then(
+                (m) => m.EventDetailComponent
+            ),
+    },
+    {
+        path: 'event-detail',
+        pathMatch: 'full',
+        redirectTo: 'home',
+    },
+    {
+        path: 'admin',
+        pathMatch: 'prefix',
+        loadComponent: () =>
+            import('./admin/admin.component').then((m) => m.AdminComponent),
+        children: [
+            {
+                path: '',
+                pathMatch: 'full',
+                redirectTo: 'sales',
+            },
+            {
+                path: 'sales',
+                pathMatch: 'full',
+                loadComponent: () =>
+                    import('./admin/sales/sales.component').then((m) => m.SalesComponent),
+            },
+            {
+                path: 'events',
+                pathMatch: 'full',
+                loadComponent: () =>
+                    import('./admin/events/events.component').then(
+                        (m) => m.EventsComponent
+                    ),
+            },
+            {
+                path: 'genres',
+                pathMatch: 'full',
+                loadComponent: () =>
+                    import('./admin/genres/genres.component').then(
+                        (m) => m.GenresComponent
+                    ),
+            },
+            {
+                path: 'reports',
+                pathMatch: 'full',
+                loadComponent: () =>
+                    import('./admin/reports/reports.component').then(
+                        (m) => m.ReportsComponent
+                    ),
+            },
+        ],
+    },
+    {
+        path: 'customer',
+        pathMatch: 'prefix',
+        loadComponent: () =>
+            import('./customer/customer.component').then((m) => m.CustomerComponent),
+        children: [
+            {
+                path: '',
+                pathMatch: 'full',
+                redirectTo: 'my-purchases',
+            },
+            {
+                path: 'my-purchases',
+                pathMatch: 'full',
+                loadComponent: () =>
+                    import('./customer/my-purchases/my-purchases.component').then(
+                        (m) => m.MyPurchasesComponent
+                    ),
+            },
+            {
+                path: 'change-password',
+                pathMatch: 'full',
+                loadComponent: () =>
+                    import('./customer/change-password/change-password.component').then(
+                        (m) => m.ChangePasswordComponent
+                    ),
+            },
+        ],
     },
 ];
