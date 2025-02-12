ffbcbfba05a6d86216b11f3af657d854e9bcf3a6
Abdal Lizano <abdallizanomuriel08@gmail.com>
2025-02-12 00:51:36 -0500

    Update imports Admin component

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { LoggedInHeaderComponent } from "../Shared/components/logged-in-header/logged-in-header/logged-in-header.component";
 import { FooterComponent } from "../Shared/components/footer/footer.component";
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-admin',
   standalone: true,
-  imports: [LoggedInHeaderComponent, FooterComponent,RouterOutlet],
+  imports: [LoggedInHeaderComponent, FooterComponent, RouterOutlet, RouterLinkActive, RouterLink],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
