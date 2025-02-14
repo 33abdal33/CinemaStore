844f95d02cb618c993071921bec514654707b7ea
AbdalLM <abdallm08@hotmail.com>
2025-02-14 15:40:56 -0500

    implement ng2-charts

--- src/app/admin/reports/reports.component.ts
+++ src/app/admin/reports/reports.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ChartConfiguration, ChartData, ChartType } from 'chart.js';
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-reports',
   standalone: true,
-  imports: [],
+  imports: [BaseChartDirective],
   templateUrl: './reports.component.html',
   styleUrl: './reports.component.css'
 })
 export class ReportsComponent {
+  @ViewChild(BaseChartDirective) chart: BaseChartDirective | undefined;
+  pieChartData: ChartData<'pie', number[], string> = {
+    labels: ['Ventas', 'Compras', 'Ganancias'],
+    datasets: [
+      {
+        data: [500, 300, 100],
+      },
+    ],
+  };
+  pieChartType: ChartType = 'pie';
 
+  loadRealData() {
+    this.pieChartData.datasets[0].data = [100, 200, 300];
+    this.chart?.update();
+  }
 }
