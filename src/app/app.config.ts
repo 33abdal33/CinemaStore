aa9398741b6359df9cb69e5e77f7fce3abebe849
Abdal Lizano <abdallizanomuriel08@gmail.com>
2025-02-14 00:28:15 -0500

    Add ng2-Charts library

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,11 +7,12 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { jwtInterceptor, loadingInterceptor, loggerInterceptor } from './app.interceptor';
 
 import { SimpleNotificationsModule } from 'angular2-notifications'
-import { NgxSpinnerModule } from 'ngx-spinner'
+import { NgxSpinnerModule } from 'ngx-spinner';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts'
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(),
   provideHttpClient(withInterceptors([loggerInterceptor, jwtInterceptor, loadingInterceptor])),
-  importProvidersFrom(SimpleNotificationsModule.forRoot(), NgxSpinnerModule),
+  importProvidersFrom(SimpleNotificationsModule.forRoot(), NgxSpinnerModule), provideCharts(withDefaultRegisterables()),
   ],
 };
